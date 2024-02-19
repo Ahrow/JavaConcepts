6202c7d13e4dc9b57edc6f87b83a34c2249b5dc5
Ahrow <jonasbergenheim90@gmail.com>
2024-02-19 12:19:13 +0100

    add Stack example

---
+++ src/main/java/algorithms_and_datastructures/stack/StackExample.java
@@ -0,0 +1,31 @@
+package algorithms_and_datastructures.stack;
+/**
+ * https://www.geeksforgeeks.org/stack-push-method-in-java/
+ * */
+import java.util.Stack;
+
+public class StackExample {
+    public static void main(String args[])
+    {
+        // Creating an empty Stack
+        Stack<Integer> STACK = new Stack<Integer>();
+
+        // Use push() to add elements into the Stack
+        STACK.push(10);
+        STACK.push(15);
+        STACK.push(30);
+        STACK.push(20);
+        STACK.push(5);
+        STACK.push(null);
+
+        // Displaying the Stack
+        System.out.println("Initial Stack: " + STACK);
+
+        // Pushing elements into the Stack
+        STACK.push(1254);
+        STACK.push(4521);
+
+        // Displaying the final Stack
+        System.out.println("Final Stack: " + STACK);
+    }
+}
